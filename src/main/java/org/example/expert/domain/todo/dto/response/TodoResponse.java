b95326e8ead75c034d42e4b81576917985c0fddc
OverNight <hidden98716@gmail.com>
2025-09-01 10:20:34 +0900

    refactor(lv6): refactor TodoResponse to record

--- src/main/java/org/example/expert/domain/todo/dto/response/TodoResponse.java
+++ src/main/java/org/example/expert/domain/todo/dto/response/TodoResponse.java
@@ -1,28 +1,22 @@
 package org.example.expert.domain.todo.dto.response;
 
 import lombok.Getter;
+import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.user.dto.response.UserResponse;
 
 import java.time.LocalDateTime;
 
-@Getter
-public class TodoResponse {
-
-    private final Long id;
-    private final String title;
-    private final String contents;
-    private final String weather;
-    private final UserResponse user;
-    private final LocalDateTime createdAt;
-    private final LocalDateTime modifiedAt;
-
-    public TodoResponse(Long id, String title, String contents, String weather, UserResponse user, LocalDateTime createdAt, LocalDateTime modifiedAt) {
-        this.id = id;
-        this.title = title;
-        this.contents = contents;
-        this.weather = weather;
-        this.user = user;
-        this.createdAt = createdAt;
-        this.modifiedAt = modifiedAt;
+public record TodoResponse (
+    Long id,
+    String title,
+    String contents,
+    String weather,
+    UserResponse user,
+    LocalDateTime createdAt,
+    LocalDateTime modifiedAt
+){
+    public static TodoResponse of(Todo todo, UserResponse user) {
+        return new TodoResponse(todo.getId(), todo.getTitle(), todo.getContents(), todo.getWeather(),
+                user, todo.getCreatedAt(), todo.getModifiedAt());
     }
 }
