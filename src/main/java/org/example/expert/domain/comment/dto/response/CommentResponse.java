3d830faac4655165c78549a1262e7b5d062fa874
OverNight <hidden98716@gmail.com>
2025-09-01 10:32:12 +0900

    refactor(lv6): refactor CommentResponse to record

--- src/main/java/org/example/expert/domain/comment/dto/response/CommentResponse.java
+++ src/main/java/org/example/expert/domain/comment/dto/response/CommentResponse.java
@@ -1,18 +1,14 @@
 package org.example.expert.domain.comment.dto.response;
 
-import lombok.Getter;
+import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.user.dto.response.UserResponse;
 
-@Getter
-public class CommentResponse {
-
-    private final Long id;
-    private final String contents;
-    private final UserResponse user;
-
-    public CommentResponse(Long id, String contents, UserResponse user) {
-        this.id = id;
-        this.contents = contents;
-        this.user = user;
+public record CommentResponse (
+    Long id,
+    String contents,
+    UserResponse user
+) {
+    public static CommentResponse of(Comment comment, UserResponse user) {
+        return new CommentResponse(comment.getId(), comment.getContents(), user);
     }
 }
