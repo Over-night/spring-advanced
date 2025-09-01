6804b9b221e1b62debdc7378a596d99c6c3aa035
OverNight <hidden98716@gmail.com>
2025-09-01 09:56:30 +0900

    refactor(lv6): revise UserResponse to record

--- src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
+++ src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
@@ -1,15 +1,12 @@
 package org.example.expert.domain.user.dto.response;
 
-import lombok.Getter;
+import org.example.expert.domain.user.entity.User;
 
-@Getter
-public class UserResponse {
-
-    private final Long id;
-    private final String email;
-
-    public UserResponse(Long id, String email) {
-        this.id = id;
-        this.email = email;
+public record UserResponse(
+        Long id,
+        String email
+) {
+    public static UserResponse of(User user) {
+        return new UserResponse(user.getId(), user.getEmail());
     }
 }
