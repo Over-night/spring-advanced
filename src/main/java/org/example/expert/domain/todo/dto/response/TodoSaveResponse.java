82a2c745703366f89af50d8a97d492871998b167
OverNight <hidden98716@gmail.com>
2025-09-01 10:15:19 +0900

    fix(lv6): extract weather getter from transaction

--- src/main/java/org/example/expert/domain/todo/dto/response/TodoSaveResponse.java
+++ src/main/java/org/example/expert/domain/todo/dto/response/TodoSaveResponse.java
@@ -1,22 +1,16 @@
 package org.example.expert.domain.todo.dto.response;
 
-import lombok.Getter;
+import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.user.dto.response.UserResponse;
 
-@Getter
-public class TodoSaveResponse {
-
-    private final Long id;
-    private final String title;
-    private final String contents;
-    private final String weather;
-    private final UserResponse user;
-
-    public TodoSaveResponse(Long id, String title, String contents, String weather, UserResponse user) {
-        this.id = id;
-        this.title = title;
-        this.contents = contents;
-        this.weather = weather;
-        this.user = user;
+public record TodoSaveResponse(
+        Long id,
+        String title,
+        String contents,
+        String weather,
+        UserResponse user
+) {
+    public static TodoSaveResponse of(Todo todo, String weather, UserResponse userResponse) {
+        return new TodoSaveResponse(todo.getId(), todo.getTitle(), todo.getContents(), weather, userResponse);
     }
 }
